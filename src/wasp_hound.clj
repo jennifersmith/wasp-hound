5b57a3b04bbc8852e55527ebe77de151c9d7b596
Jennifer Smith <jennifer@home>
2014-04-27 07:42:54 +1000

    Now detecting magick keys

--- src/wasp_hound.clj
+++ src/wasp_hound.clj
@@ -1,6 +1,14 @@
 (ns wasp-hound
   (:gen-class))
 
+
+
+(defn detect-magic-keys [code]
+  (->> code
+   (tree-seq coll? seq)
+   (filter keyword?)
+   (frequencies)))
+
 (defn -main
   [x]
   (println x "Hello, World!"))
--- test/wasp_hound_test.clj
+++ test/wasp_hound_test.clj
@@ -2,5 +2,14 @@
   (:use midje.sweet
         wasp-hound))
 
+(def code-snippet 
+  '(defn foo {:foo 1 :bar 2}))
 
-(fact 1  => 1)
+(def second-code-snippet
+  '(defn dupe {:foo 1}))
+
+(fact "parses all the code we give it and produces a report about the usage of keywords"
+      (detect-magic-keys code-snippet) => {:foo 1 :bar 1})
+
+(fact "copes with multiple defs"
+      (detect-magic-keys (vector code-snippet second-code-snippet)) => {:foo 2 :bar 1})
